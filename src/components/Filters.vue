23723dae1c8c65e37a1b06472c84d966843cb074
Anna <annagiulia.musso@gmail.com>
2021-12-06 14:00:01 +0100

    initialied filters component

---
+++ src/components/Filters.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-select
+          :items="cathegories"
+          label="Nach Kategorie Sortieren"
+        ></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Filters",
+  computed: {
+    cathegories() {
+      return this.$store.state.cathegories;
+    },
+  },
+};
+</script>
+
+<style></style>

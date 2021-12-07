a31989919eee457c0f9e81b44511db90b08c7b54
Anna <annagiulia.musso@gmail.com>
2021-12-07 09:28:41 +0100

    finalized post editing

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -142,8 +142,9 @@ export default {
       this.setPostId();
       this.setDate();
       const finalData = { ...this.postData };
+      console.log(finalData);
       if (this.post) {
-        this.$store.dispatch("editPost", this.post);
+        this.$store.dispatch("editPost", finalData);
       } else {
         this.$store.dispatch("updateMessage", finalData);
       }

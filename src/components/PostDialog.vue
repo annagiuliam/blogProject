a8d316023cbd24978c71e186a8decb217f065f4e
Anna <annagiulia.musso@gmail.com>
2021-12-08 11:56:24 +0100

    changed to formatted Date

--- src/components/PostDialog.vue
+++ src/components/PostDialog.vue
@@ -37,7 +37,9 @@
                 <v-list-item-subtitle>{{ post.author }}</v-list-item-subtitle>
               </v-col>
               <v-col>
-                <v-list-item-subtitle>{{ post.date }}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{
+                  post.formattedDate
+                }}</v-list-item-subtitle>
               </v-col>
             </v-row>
           </v-list-item-content>

af7dac54a725fafeab30f78d8c38b23f553d41d1
Anna <annagiulia.musso@gmail.com>
2021-12-15 11:27:52 +0100

    code cleanup

--- src/components/PostTile.vue
+++ src/components/PostTile.vue
@@ -1,19 +1,16 @@
 <template>
   <v-card>
     <PostContent :post="post" :elip="elip" />
-    <!-- <PostDialog :post="post" v-if="elip" /> -->
     <v-btn @click="openDialog('PostDialog')"> read </v-btn>
   </v-card>
 </template>
 
 <script>
-// import PostDialog from "./PostDialog.vue";
 import PostContent from "./PostContent.vue";
 export default {
   name: "PostTile",
   props: ["post", "elip"],
   components: {
-    // PostDialog,
     PostContent,
   },
   methods: {
